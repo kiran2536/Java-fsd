32133cc16518ff2ba72b0534fdaade1566f7dd5d
kiran2536 <144305609+kiran2536@users.noreply.github.com>
2023-12-30 21:09:45 +0530

    Add files via upload

---
+++ eBookStore-Config-Server/src/test/java/com/mphasis/eBookStoreConfigServer/EBookStoreConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mphasis.eBookStoreConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookStoreConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
